d17524201f19cca25f66e3a43e0229632a72a546
Notavone <notavone@gmail.com>
2022-02-19 21:18:57 +0100

    sort products by name

--- src/app/products/product-table/product-table.component.ts
+++ src/app/products/product-table/product-table.component.ts
@@ -1,17 +1,23 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {Product} from "../shared/product";
 import {ProductService} from '../shared/product.service';
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-product-table',
   templateUrl: './product-table.component.html',
   styleUrls: ['./product-table.component.scss']
 })
-export class ProductTableComponent {
+export class ProductTableComponent implements OnChanges {
   @Input() products: Product[] = [];
   @Input() columnsToDisplay: string[] = ["name", "available", "price", "discount"]
+  dataSet: MatTableDataSource<Product> = new MatTableDataSource<Product>();
 
   constructor(private productService: ProductService) {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.dataSet.data = this.products.sort((a, b) => a.name.localeCompare(b.name));
   }
 
   toggleAvailability(product: Product) {
